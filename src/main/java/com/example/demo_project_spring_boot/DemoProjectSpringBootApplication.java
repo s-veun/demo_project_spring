515d02c29294e938ae66a62a17819c15c25edabe
s-veun <saveun2032@gmail.com>
2026-04-26 16:06:30 +0700

    up push

--- src/main/java/com/example/demo_project_spring_boot/DemoProjectSpringBootApplication.java
+++ src/main/java/com/example/demo_project_spring_boot/DemoProjectSpringBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoProjectSpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoProjectSpringBootApplication.class, args);
     }
-
 }
